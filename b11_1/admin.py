77f949d60cea92aab00b48251de60a77d00aa5c4
ycrausaz <yann.crausaz@gmail.com>
2024-06-23 23:27:47 +0200

    Added the files to populate the database.

--- b11_1/admin.py
+++ b11_1/admin.py
@@ -1,3 +1,56 @@
-from django.contrib import admin
+# myapp/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Material, BEGRU, Basismengeneinheit, Materialart, Sparte, Materialzustandsverwaltung, Rueckfuehrungscode, Serialnummerprofil, SparePartClassCode, Uebersetzungsstatus
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('positions_nr', 'kurztext_de', 'kurztext_fr', 'kurztext_en', 'grunddatentext_de_1_zeile')
+
+
+@admin.register(BEGRU)
+class BEGRUAdmin(admin.ModelAdmin):
+    list_display = ('text',)
+
+
+@admin.register(Basismengeneinheit)
+class BasismengeneinheitAdmin(admin.ModelAdmin):
+    list_display = ('text',)
+
+
+@admin.register(Materialart)
+class MaterialartAdmin(admin.ModelAdmin):
+    list_display = ('text',)
+
+
+@admin.register(Sparte)
+class SparteAdmin(admin.ModelAdmin):
+    list_display = ('text',)
+
+
+@admin.register(Materialzustandsverwaltung)
+class MaterialzustandsverwaltungAdmin(admin.ModelAdmin):
+    list_display = ('text',)
+
+
+@admin.register(Rueckfuehrungscode)
+class RueckfuehrungscodeAdmin(admin.ModelAdmin):
+    list_display = ('text',)
+
+
+@admin.register(Serialnummerprofil)
+class SerialnummerprofilAdmin(admin.ModelAdmin):
+    list_display = ('text',)
+
+
+@admin.register(SparePartClassCode)
+class SparePartClassCodeAdmin(admin.ModelAdmin):
+    list_display = ('text',)
+
+
+@admin.register(Uebersetzungsstatus)
+class UebersetzungsstatusAdmin(admin.ModelAdmin):
+    list_display = ('text',)
+
+
+
