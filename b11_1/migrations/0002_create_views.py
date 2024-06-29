e6a32bfd31acd95ef881d31188f3429fc55deb96
ycrausaz <yann.crausaz@gmail.com>
2024-06-29 23:42:26 +0200

    Added the code for the 'MVKE-Verkaufsdaten' view.

--- b11_1/migrations/0002_create_views.py
+++ b11_1/migrations/0002_create_views.py
@@ -1,18 +1,17 @@
 from django.db import migrations, models
 from django.db import connection
 
-def create_views_MARA(apps, schema_editor):
-    with connection.cursor() as cursor:
-        cursor.execute('''
-DROP VIEW IF EXISTS MARA;
-CREATE VIEW MARA AS
+def create_views_MARA_Grunddaten(apps, schema_editor):
+    view_sql = '''
+DROP VIEW IF EXISTS MARA_Grunddaten;
+CREATE VIEW MARA_Grunddaten AS 
 SELECT
     a.positions_nr AS SOURCE_ID,
     b.text AS MTART,
     c.text AS MEINS,
     a.herstellerteilenummer AS MFRPN,
     a.hersteller_nr_gp AS MFRNR,
-    a.groesse_abmessung AS GROES,
+    a.groesse_abmessung AS GROES, 
     a.nettogewicht AS NTGEW,
     a.laenge AS LAENG,
     a.breite AS BREIT,
@@ -56,11 +55,29 @@ join b11_1_rueckfuehrungscode h on h.id = a.rueckfuehrungscode_id
 join b11_1_sparepartclasscode i on i.id = a.spare_part_class_code_id
 join b11_1_temperaturbedingung j on j.id = a.temperaturbedingung_id
 join b11_1_sonderablauf k on k.id = a.sonderablauf_id;
-        ''')
+        '''
+    schema_editor.execute(view_sql)
 
-def drop_views_MARA(apps, schema_editor):
-    with connection.cursor() as cursor:
-        cursor.execute('DROP VIEW IF EXISTS MARA;')
+def drop_views_MARA_Grunddaten(apps, schema_editor):
+    schema_editor.execute('DROP VIEW IF EXISTS MARA_Grunddaten;')
+
+def create_views_MVKE_Verkaufsdaten(apps, schema_editor):
+    view_sql = '''
+DROP VIEW IF EXISTS MVKE_Verkaufsdaten;
+CREATE VIEW MVKE_Verkaufsdaten AS
+SELECT
+    a.positions_nr AS SOURCE_ID,
+    'M100' AS VKORG,
+    b.text AS VTWEG,
+    c.text AS MTPOS
+FROM b11_1_material a
+join b11_1_vertriebsweg b on b.id = a.vertriebsweg_id
+join b11_1_allgemeinepositionstypengruppe c on c.id = a.allgemeine_positionstypengruppe_id;
+        '''
+    schema_editor.execute(view_sql)
+
+def drop_views_MVKE_Verkaufsdaten(apps, schema_editor):
+    schema_editor.execute('DROP VIEW IF EXISTS MVKE_Verkaufsdaten;')
 
 class Migration(migrations.Migration):
 
@@ -69,9 +86,10 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL(
-            sql=create_views_MARA,
-            reverse_sql=drop_views_MARA,
-        ),
+#        migrations.RunSQL(
+#            sql=create_views_MARA_Grunddaten,
+#            reverse_sql=drop_views_MARA_Grunddaten,
+#        ),
+        migrations.RunPython(create_views_MVKE_Verkaufsdaten, reverse_code=drop_views_MVKE_Verkaufsdaten),
     ]
 
