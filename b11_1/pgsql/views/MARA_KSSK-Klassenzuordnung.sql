bc9f4938a0fb7d685e0d74bcfc25299fa4dfea68
ycrausaz <yann.crausaz@gmail.com>
2024-07-01 22:09:20 +0200

    Added the view 'MARA_KSSK-Klassenzuordnung' to the export.

---
+++ b11_1/pgsql/views/MARA_KSSK-Klassenzuordnung.sql
@@ -0,0 +1,38 @@
+-- Create a view named "MARA_AUSP_Klassenzuordnung"
+DROP VIEW IF EXISTS MARA_AUSP_Klassenzuordnung;
+CREATE VIEW MARA_AUSP_Klassenzuordnung AS
+WITH source_data AS (
+    SELECT a.positions_nr AS source_id,
+           '001' AS klart
+    FROM b11_1_material a
+    WHERE a.is_transferred = 't'
+)
+SELECT source_id, 
+       klart, 
+       'V_VERTEILUNG_PSD' AS class
+FROM source_data
+UNION ALL
+SELECT source_id, 
+       klart, 
+       'V_ZUSATZDATEN' AS class
+FROM source_data
+UNION ALL
+SELECT source_id, 
+       klart, 
+       'V_AR_NUMMER' AS class
+FROM source_data
+UNION ALL
+SELECT source_id, 
+       klart, 
+       'V_VERTEILUNG' AS class
+FROM source_data
+UNION ALL
+SELECT source_id, 
+       klart, 
+       'V_BM_SBM' AS class
+FROM source_data;
+
+-- Now you can query the view just like a table
+-- Example query to get all rows from the view
+--SELECT * FROM MARA_AUSP_Klassenzuordnung;
+
