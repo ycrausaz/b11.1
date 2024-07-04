98f47db46c4a14ea982a50bf34cbd3fbef634f33
ycrausaz <yann.crausaz@gmail.com>
2024-07-04 09:17:15 +0200

    Added an app icon.

--- LBA/settings.py
+++ LBA/settings.py
@@ -123,6 +123,7 @@ LOGIN_URL = 'login-user'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
