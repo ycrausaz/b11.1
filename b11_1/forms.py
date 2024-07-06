e426aaa2bca81ed4aaac3af85422a9f07af50d7e
ycrausaz <yann.crausaz@gmail.com>
2024-07-06 13:36:02 +0200

    Added all the fields to the 'forms.py' definition.

--- b11_1/forms.py
+++ b11_1/forms.py
@@ -7,7 +7,7 @@ from django.db import connection
 from django.urls import reverse_lazy
 from django.forms import DateField
 from django.conf import settings
-from .models import Basismengeneinheit
+from .models import Basismengeneinheit, Gefahrgutkennzeichen
 
 def readonly_field_style():
     styles_string = ' '
@@ -58,24 +58,43 @@ class MaterialForm_IL(ModelForm):
     kurztext_fr = forms.CharField(required=True)
     kurztext_en = forms.CharField(required=True)
     grunddatentext_de_1_zeile = forms.CharField(required=True)
+    grunddatentext_de_2_zeile = forms.CharField(required=False)
     grunddatentext_fr_1_zeile = forms.CharField(required=True)
+    grunddatentext_fr_2_zeile = forms.CharField(required=False)
     grunddatentext_en_1_zeile = forms.CharField(required=True)
+    grunddatentext_en_2_zeile = forms.CharField(required=False)
     basismengeneinheit = forms.ModelChoiceField(queryset=Basismengeneinheit.objects.all(), required=True)
     bruttogewicht = forms.IntegerField(required=True)
+    nettogewicht = forms.IntegerField(required=False)
+    groesse_abmessung = forms.IntegerField(required=False)
+    ean_upc_code = forms.CharField(required=False)
+    nato_stock_number = forms.CharField(required=False)
+    nsn_gruppe_klasse = forms.CharField(required=False)
+    nato_versorgungs_nr = forms.CharField(required=False)
     herstellerteilenummer = forms.CharField(required=True)
+    normbezeichnung = forms.CharField(required=False)
+    gefahrgutkennzeichen = forms.CharField(required=False)
+    gefahrgutkennzeichen = forms.ModelChoiceField(queryset=Gefahrgutkennzeichen.objects.all(), required=False)
     instandsetzbar = CustomBooleanChoiceField(required=True)
     chargenpflicht = CustomBooleanChoiceField(required=True)
+    bestellmengeneinheit = forms.IntegerField(required=False)
+    mindestbestellmenge = forms.IntegerField(required=False)
     lieferzeit = forms.IntegerField(required=True)
-    einheit_l_b_h = forms.CharField(required=True)
-    laenge = forms.CharField(required=True)
-    breite = forms.CharField(required=True)
-    hoehe = forms.CharField(required=True)
-    preis = forms.CharField(required=True)
-    preiseinheit = forms.CharField(required=True)
+    einheit_l_b_h = forms.IntegerField(required=True)
+    laenge = forms.IntegerField(required=True)
+    breite = forms.IntegerField(required=True)
+    hoehe = forms.IntegerField(required=True)
+    preis = forms.IntegerField(required=True)
+    preiseinheit = forms.IntegerField(required=True)
+    lagerfaehigkeit = forms.IntegerField(required=True)
+    exportkontrollauflage = CustomBooleanChoiceField(required=False)
+    cage_code = forms.CharField(required=False)
     hersteller_name = forms.CharField(required=True)
     hersteller_adresse = forms.CharField(required=True)
-    hersteller_plz = forms.CharField(required=True)
+    hersteller_plz = forms.IntegerField(required=True)
     hersteller_ort = forms.CharField(required=True)
+    revision = forms.CharField(required=False)
+    bemerkung = forms.CharField(required=False)
 
     class Meta:
         model = Material
