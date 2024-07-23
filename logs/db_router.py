9b681b9065c97718edef1bb9a73f9dcfc0831121
ycrausaz <yann.crausaz@gmail.com>
2024-07-23 18:48:54 +0200

    Set the logging to be sent to the Django console and to the database.

--- logs/db_router.py
+++ logs/db_router.py
@@ -1,25 +1,18 @@
-# logs/db_router.py
-
 class LogsDbRouter:
-    """
-    A router to control all database operations on models in the
-    logs application.
-    """
-
     def db_for_read(self, model, **hints):
         if model._meta.app_label == 'logs':
             return 'logs_db'
-        return 'default'
+        return None
 
     def db_for_write(self, model, **hints):
         if model._meta.app_label == 'logs':
             return 'logs_db'
-        return 'default'
+        return None
 
     def allow_relation(self, obj1, obj2, **hints):
-        return True
+        return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         if app_label == 'logs':
             return db == 'logs_db'
-        return db == 'default'
+        return None
