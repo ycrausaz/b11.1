1727f5883d1184229811d9a09fae820462b252f2
ycrausaz <yann.crausaz@gmail.com>
2024-11-10 19:13:46 +0100

    Further steps in the implementation of the addition of logical indexes: the fields corresponding the foreign key become a value when loading the dedicated form.

--- b11_1/widgets.py
+++ b11_1/widgets.py
@@ -4,13 +4,36 @@ from django.utils.safestring import mark_safe
 from .utils import readonly_field_style
 
 class ReadOnlyForeignKeyWidget(widgets.Widget):
+    def __init__(self, attrs=None, choices=()):
+        super().__init__(attrs)
+        self.choices = choices
+
+    def value_from_datadict(self, data, files, name):
+        value = data.get(name, None)
+        return value
+
     def render(self, name, value, attrs=None, renderer=None):
         if attrs is None:
             attrs = {}
         if value is None:
             return ''
-        foreign_key_instance = self.choices.queryset.get(idx=value)
-        attrs['style'] = readonly_field_style()
-        rendered_html = '<input type="text" value="%s" readonly style="%s"/>' % (foreign_key_instance.__str__(), attrs['style'])
-        return mark_safe(rendered_html)
 
+        # Get the related model
+        model = self.choices.queryset.model if hasattr(self.choices, 'queryset') else None
+        if model is None:
+            return ''
+
+        try:
+            # Try to get the instance using the foreign key relationship
+            if hasattr(model, 'idx'):
+                instance = model.objects.get(idx=value)
+            else:
+                instance = model.objects.get(pk=value)
+            
+            display_value = str(instance)
+        except (model.DoesNotExist, AttributeError, ValueError):
+            display_value = ''
+
+        attrs['style'] = readonly_field_style()
+        final_attrs = self.build_attrs(attrs, {'type': 'text', 'name': name, 'value': display_value, 'readonly': 'readonly'})
+        return mark_safe('<input%s />' % widgets.flatatt(final_attrs))
