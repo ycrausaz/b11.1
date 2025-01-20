b7310086b2d0cf3840c37b78cc1140d32be52d2c
ycrausaz <yann.crausaz@gmail.com>
2025-01-20 22:14:49 +0100

    Updated '_full_build_and_start.sh', so that the process doesn't end up with a failure when loading the backup.

--- _full_build_and_start.sh
+++ _full_build_and_start.sh
@@ -1,5 +1,4 @@
-docker-compose build --no-cache --progress=plain && docker-compose up -d
-sleep 20
+docker-compose build --no-cache --progress=plain && docker-compose up --wait -d
 docker compose run --rm web python manage.py loaddata b11_1/pgsql/backup_users_and_groups.json
 docker compose run --rm web python manage.py create_user_profiles
 docker compose run --rm web python manage.py populate_db
