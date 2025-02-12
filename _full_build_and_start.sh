34c27ed35d3c8a22ee9e06f29db7443aa0240794
ycrausaz <yann.crausaz@gmail.com>
2025-02-12 17:52:53 +0100

    Updated '_full_build_and_start.sh' to fit to 'symm'.

--- _full_build_and_start.sh
+++ _full_build_and_start.sh
@@ -1,5 +1,5 @@
 docker-compose build --no-cache --progress=plain && docker-compose up --wait -d
-docker compose run --rm web python manage.py loaddata b11_1/pgsql/backup_users_and_groups.json
+docker compose run --rm web python manage.py loaddata symm/pgsql/backup_users_and_groups.json
 docker compose run --rm web python manage.py create_user_profiles
 docker compose run --rm web python manage.py populate_db
 docker compose run --rm web python manage.py populate_db_data
