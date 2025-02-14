c33860b1297e6169b04394468ae4c718da4765cc
ycrausaz <yann.crausaz@gmail.com>
2025-02-14 18:11:10 +0100

    Updated '_stop_and_clean.sh' to impact only the volumes, containers and images having 'lba' in their name.

--- _stop_and_clean.sh
+++ _stop_and_clean.sh
@@ -1,2 +1,1 @@
-docker stop $(docker ps -a --filter "name=lba" -q) && docker rm $(docker ps -a --filter "name=lba" -q) && docker rmi $(docker images --filter=reference="*lba*" -q)
-) && docker volume rm $(docker volume ls -q --filter name=lba)
+docker stop $(docker ps -a --filter "name=lba" -q) && docker rm $(docker ps -a --filter "name=lba" -q) && docker rmi $(docker images --filter=reference="*lba*" -q) && docker volume rm $(docker volume ls -q --filter name=lba)
