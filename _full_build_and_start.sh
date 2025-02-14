ce459e297549b74828325661e4cfbac846286b7c
ycrausaz <yann.crausaz@gmail.com>
2025-02-14 17:30:23 +0100

    Updated '_full_build_and_start.sh' to 'symm'.

--- _full_build_and_start.sh
+++ _full_build_and_start.sh
@@ -3,4 +3,4 @@ docker compose run --rm web python manage.py loaddata symm/pgsql/backup_users_an
 docker compose run --rm web python manage.py create_user_profiles
 docker compose run --rm web python manage.py populate_db
 docker compose run --rm web python manage.py populate_db_data
-docker compose run --rm web cat b11_1/version.txt
+docker compose run --rm web cat symm/version.txt
