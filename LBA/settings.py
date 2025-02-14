1aa92df91f3d4f0cc611143e9066fb24f73650eb
ycrausaz <yann.crausaz@gmail.com>
2025-02-14 18:50:38 +0100

    Configured 'settings.py' for the Divio environment (source: https://docs.divio.com/reference/work-media-storage/).

--- LBA/settings.py
+++ LBA/settings.py
@@ -169,12 +169,36 @@ LOCALE_PATHS = [
 # S3/MinIO settings
 if DIVIO_HOSTING:
     # Production settings (Divio S3)
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = os.environ.get('AWS_REGION_NAME', 'us-east-1')
-    AWS_S3_USE_SSL = True
-    AWS_QUERYSTRING_AUTH = True
+    # source: https://docs.divio.com/reference/work-media-storage/
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('DEFAULT_STORAGE_BUCKET', '')
+    AWS_ACCESS_KEY_ID = os.environ.get('DEFAULT_STORAGE_ACCESS_KEY_ID', '')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('DEFAULT_STORAGE_SECRET_ACCESS_KEY', '')
+    AWS_S3_CUSTOM_DOMAIN = os.environ.get('DEFAULT_STORAGE_CUSTOM_DOMAIN', '')
+    AWS_S3_REGION_NAME = os.environ.get('DEFAULT_STORAGE_REGION', '')
+    AWS_S3_OBJECT_PARAMETERS = {
+        'ACL': 'public-read',
+        'CacheControl': 'max-age=86400',
+    }
+    AWS_S3_FILE_OVERWRITE = False
+    
+    # Default storage settings, with the staticfiles storage updated.
+    # See https://docs.djangoproject.com/en/5.1/ref/settings/#std-setting-STORAGES
+    STORAGE_BACKEND = "django.core.files.storage.FileSystemStorage"
+    
+    if AWS_SECRET_ACCESS_KEY:
+      STORAGE_BACKEND = "storages.backends.s3boto3.S3Boto3Storage"
+    
+    STORAGES = {
+        "default": {
+            "BACKEND": STORAGE_BACKEND,
+        },
+        # ManifestStaticFilesStorage is recommended in production, to prevent
+        # outdated JavaScript / CSS assets being served from cache
+        # See https://docs.djangoproject.com/en/5.1/ref/contrib/staticfiles/#manifeststaticfilesstorage
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
+        },
+    }
 else:
     # Development settings (MinIO)
     AWS_ACCESS_KEY_ID = 'admin'
