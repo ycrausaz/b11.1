ac08f6618fbff801dcead1d9b4cfb8e58f258927
ycrausaz <yann.crausaz@gmail.com>
2025-03-17 08:38:55 +0100

    Updated '_full_build_and_start.sh' from 'symm' to 'b11_1'.

--- _full_build_and_start.sh
+++ _full_build_and_start.sh
@@ -1,6 +1,6 @@
 docker-compose build --no-cache --progress=plain && docker-compose up --wait -d
-docker compose run --rm web python manage.py loaddata symm/pgsql/backup_users_and_groups.json
+docker compose run --rm web python manage.py loaddata b11_1/pgsql/backup_users_and_groups.json
 docker compose run --rm web python manage.py create_user_profiles
 docker compose run --rm web python manage.py populate_db
 docker compose run --rm web python manage.py populate_db_data
-docker compose run --rm web cat symm/version.txt
+docker compose run --rm web cat b11_1/version.txt
