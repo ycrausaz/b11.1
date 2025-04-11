aafc7d65f4ccecf2be4ce3825e9e21c0320f0515
ycrausaz <yann.crausaz@gmail.com>
2025-04-11 12:27:49 +0200

    Added 'kautb' and 'sernp' to the view 'marc_werksdaten'.

--- b11_1/pgsql/views/MARC_-_Werksdaten.sql
+++ b11_1/pgsql/views/MARC_-_Werksdaten.sql
@@ -5,7 +5,9 @@ CREATE OR REPLACE VIEW marc_werksdaten
     a.lieferzeit AS plifz,
     b.text AS werks,
     'ND'::text AS dismm,
+    'X'::text AS kautb,
     c.text AS fevor,
+    d.text AS sernp,
         CASE
             WHEN a.orderbuchpflicht = true THEN 'X'::text
             ELSE NULL::text
@@ -13,5 +15,6 @@ CREATE OR REPLACE VIEW marc_werksdaten
    FROM b11_1_material a
      LEFT JOIN b11_1_werkzuordnung_1 b ON a.werkzuordnung_1_id = b.idx
      LEFT JOIN b11_1_fertigungssteuerer c ON a.fertigungssteuerer_id = c.idx
+     LEFT JOIN b11_1_serialnummerprofil d ON a.serialnummerprofil_id = d.idx
   WHERE a.is_transferred = true
   ORDER BY a.positions_nr, b.text;
