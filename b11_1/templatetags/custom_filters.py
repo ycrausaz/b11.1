eeaf2a8f048803915db3ceaad1315e2f29a2b889
ycrausaz <yann.crausaz@gmail.com>
2025-05-10 17:06:35 +0200

    Updated the templates 'show_material_*.html', so that the fields set as foreign keys display the linked value and not only its id.

--- b11_1/templatetags/custom_filters.py
+++ b11_1/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db import models
 import os
 
 register = template.Library()
@@ -6,3 +7,21 @@ register = template.Library()
 @register.filter
 def basename(value):
     return os.path.basename(value)
+
+@register.filter
+def get_display_value(material, field_name):
+    """
+    Get the appropriate display value for a field, 
+    handling foreign keys and other special cases
+    """
+    value = getattr(material, field_name, None)
+    
+    # Handle foreign key relationships
+    if value and isinstance(value, models.Model):
+        return str(value)
+    
+    # Handle None values
+    if value is None:
+        return ""
+    
+    return value
